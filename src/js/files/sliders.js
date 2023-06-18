0ac5c9af075f129c7526282f8bb91eeeec458e68
Pauchoook <pauchoook@mail.ru>
2023-06-18 12:19:18 +0300

    specialists

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,6 +1,6 @@
-import Swiper, { Navigation } from "swiper";
+import Swiper, { Navigation, Scrollbar } from "swiper";
 
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Scrollbar]);
 
 export default function sliders() {
   const aboutSlider = document.querySelector(".about__slider");
@@ -16,6 +16,27 @@ export default function sliders() {
       navigation: {
         nextEl: " .about__slider-btn--next",
         prevEl: " .about__slider-btn--prev",
+      },
+      breakpoints: {
+        993: {
+          spaceBetween: 16,
+        },
+      },
+    });
+  }
+
+  const specialistsSlider = document.querySelector(".specialists__slider");
+
+  if (specialistsSlider) {
+    const swiper = new Swiper(specialistsSlider, {
+      speed: 700,
+      grabCursor: true,
+      slidesPerView: "auto",
+      spaceBetween: 10,
+      initialSlide: 1,
+      scrollbar: {
+        el: ".specialists__slider-scrollbar",
+        draggable: true 
       },
       breakpoints: {
         993: {
